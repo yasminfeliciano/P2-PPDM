ec9697348e7e4b478a8737647ae6c98ed80da2ee
Dracozauberer <vsbarcellos20@gmail.com>
2026-05-26 21:02:20 -0300

    fetch(p2): tipos  da aplicação

--- pesquisa-de-paises/App.tsx
+++ pesquisa-de-paises/App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+type ResultadoNome = {
+  nomeComum: string;
+  nomeOficial: string;
+  nomeRusso: string;
+  openStreetMaps: string;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+type ResultadoCapital = {
+  nomeOficial: string;
+  bandeiraPng: string;
+};
